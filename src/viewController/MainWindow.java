eee9b3a3e7219956dd98b01a46c432cb6478efa3
Unknown <sagsdf_github_tyler71@xyzz.work>
2020-03-08 01:52:08 -0700

    main window: Add fx:ids to fxml and controller

--- src/viewController/MainWindow.java
+++ src/viewController/MainWindow.java
@@ -1,5 +1,37 @@
 package viewController;
 
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
 public class MainWindow {
+    @FXML
+    private TableView appointmentView;
+    @FXML
+    private TextField consultantIDField;
+    @FXML
+    private DatePicker consultantAppointmentDate;
+    @FXML
+    private RadioButton businessRadio;
+    @FXML
+    private RadioButton PersonalRadio;
+    @FXML
+    private TextField customerIDField;
+    @FXML
+    private CheckBox enableLoggingCheck;
+    @FXML
+    private CheckBox calendarMonthViewCheck;
+
+
+    @FXML
+    private void addAppointment() {
+    }
+
+    @FXML
+    private void updateAppointment() {
+    }
+
+    @FXML
+    private void deleteAppointment() {
+    }
 }
