9829fe4aef4dc68893d8fcd49618275a22ad6ce1
Unknown <sagsdf_github_tyler71@xyzz.work>
2020-03-16 07:26:38 -0700

    datetime conversion more abstract

--- src/misc/DateTimeConversion.java
+++ src/misc/DateTimeConversion.java
@@ -1,5 +1,6 @@
 package misc;
 
+import dataModel.DataSource;
 import dataModel.DataSourceSourceMySql;
 
 import java.time.*;
@@ -7,14 +8,14 @@ import java.time.*;
 public class DateTimeConversion {
 
     public ZonedDateTime parseMysqlSQLTime(String time) {
-        ZonedDateTime converted = LocalDateTime.parse(time, DataSourceSourceMySql.dateTimeFormatter)
+        ZonedDateTime converted = LocalDateTime.parse(time, DataSource.getDb().getDateTimeFormatter())
                 .atOffset(ZoneOffset.UTC)
                 .atZoneSameInstant(ZoneId.systemDefault());
         return converted;
     }
     public String convertToMysqlSqlTime(ZonedDateTime dateTimeObject) {
         ZonedDateTime utcZoned = dateTimeObject.withZoneSameInstant(ZoneOffset.UTC);
-        String convertedTime = DataSourceSourceMySql.dateTimeFormatter.format(utcZoned);
+        String convertedTime = DataSource.getDb().getDateTimeFormatter().format(utcZoned);
         return convertedTime;
     }
 
