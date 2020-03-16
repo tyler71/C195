1e60b3aaef9f58e682efc655ef64b91d240d3030
Unknown <sagsdf_github_tyler71@xyzz.work>
2020-03-16 15:36:44 -0700

    init reports controllers

--- src/viewController/Reports.java
+++ src/viewController/Reports.java
@@ -1,4 +1,23 @@
 package viewController;
 
+import javafx.fxml.FXML;
+
 public class Reports {
+
+    @FXML
+    public void handleAppointmentTypesMonth() {
+//        TODO  number of appointment types by month
+//        For every month, show how many appointments of each type
+
+    }
+
+    @FXML
+    public void handleSchedulesPerConsultant() {
+
+    }
+
+    @FXML
+    public void handleReportCustomersPerConsultant() {
+
+    }
 }
